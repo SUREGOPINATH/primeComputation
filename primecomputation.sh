ff9d1b1a92aa4e187d40e8cb212052731d2f4209
SURE GOPINATH <suregopinath@gmail.com>
2020-10-11 12:09:33 +0530

    first hundred in reverse order is done

--- primecomputation.sh
+++ primecomputation.sh
@@ -1,3 +1,65 @@
 #!/bin/bash
 
 echo "welcome to prime computation"
+
+function intialise() {
+        count=0
+        index=0
+        prime=2
+        declare -a primeArray
+
+}
+
+function checkPrime() {
+
+        flag=0
+        upper=$(( $1/2 ))
+        for (( counter=2;counter<=$upper;counter++ ))
+        do
+                if [ $(($1%$counter)) -eq 0 ]
+                then
+                        flag=1
+                        break
+                fi
+        done
+        echo $flag
+}
+
+function printArray() {
+
+        echo "first 100 prime number are : "
+   for (( counter=$(($index-1)); counter>=0; counter-- ))
+   do
+      echo ${primeArray[$counter]}
+   done
+
+}
+
+function firstHundredPrime() {
+
+        while [ $count -lt 100 ]
+        do
+                primeNumber=$( checkPrime $prime )
+		if [ $primeNumber -eq 0 ]
+		then
+	                primeArray[$index]=$prime
+			index=$(($index+1))
+        	        prime=$(($prime+1))
+			count=$(($count+1))
+		else
+			prime=$(($prime+1))
+		fi
+        done
+        printArray
+}
+
+function primeComputation() {
+
+        intialise;
+	firstHundredPrime;
+
+        ##firstFiftyAlterPrime;
+##      firstHundredWithUnitPlace;
+}
+
+primeComputation;
